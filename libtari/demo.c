7b359a582e8ca77c3f818691a67949fa499152b8
CjS77 <CjS77@users.noreply.github.com>
2020-10-14 20:46:07 +0200

    Super simple Ristretto FFI
    This PR adds a very barebones FFI support to the crate.
    No error checking!
    All memory managed by caller!
    No types exported across the FFI boundary!
    
    A demo program and Makefile is presented to see how it all fits
    together.

--- libtari/demo.c
+++ libtari/demo.c
@@ -1,8 +1,47 @@
 #include "tari_crypto.h"
 #include <stdio.h>
 
+void print_key(uint8_t key[]) {
+  int i;
+  for (i = 0; i < KEY_LENGTH; i++) {
+    printf("%02X", key[i]);
+  }
+  printf("\n");
+}
+
+/*
+ * This demo generates a key pair, signs a message and then validates the signature.
+ * All memory in this FFI is managed by the caller. In this demo, the data is kept on the stack, and so explicit
+ * memory management is not done, but in general, you have to allocate and free memory yourself.
+ */
 int main() {
     const char *ver = version();
     printf("Tari Crypto (v%s)\n", ver);
+
+    uint8_t pub_key[KEY_LENGTH], priv_key[KEY_LENGTH];
+
+    random_keypair(&priv_key, &pub_key);
+    printf("Keys generated\n");
+    print_key(priv_key);
+    print_key(pub_key);
+
+    // Sign and verify message
+    const char msg[] = "Hello world";
+
+    uint8_t r[KEY_LENGTH], sig[KEY_LENGTH];
+
+    sign(&priv_key, &msg, &r, &sig);
+
+    printf("Signed message\n");
+    print_key(r);
+    print_key(sig);
+
+    printf("Check signature: ");
+    if (verify(&pub_key, &msg, &r, &sig)) {
+        printf("SUCCESS\n");
+    } else {
+        printf("FAILED\n");
+    }
     return 0;
 }
+
--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -15,7 +15,16 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use crate::{
+    hash::blake2::Blake256,
+    keys::{PublicKey, SecretKey},
+    ristretto::{RistrettoPublicKey, RistrettoSchnorr, RistrettoSecretKey},
+};
+use digest::Digest;
 use libc::c_char;
+use rand::rngs::OsRng;
+use std::ffi::CStr;
+use tari_utilities::ByteArray;
 
 const VERSION: &str = concat!(env!("CARGO_PKG_VERSION"), "\u{00}");
 
@@ -24,7 +33,95 @@ pub extern "C" fn version() -> *const c_char {
     VERSION.as_ptr() as *const c_char
 }
 
+const KEY_LENGTH: usize = 32;
+
+type KeyArray = [u8; KEY_LENGTH];
+
+/// Generate a new key pair and copies the values into the provided arrays.
+///
+/// If `pub_key` is null, then only a private key is generated.
+/// The *caller* must manage memory for the results. Besides checking for null values, this function assumes that at
+/// least `KEY_LENGTH` bytes have been allocated in `priv_key` and `pub_key`.
 #[no_mangle]
-pub extern "C" fn hello(a: usize) -> usize {
-    2 * a
+pub unsafe extern "C" fn random_keypair(priv_key: *mut KeyArray, pub_key: *mut KeyArray) {
+    if priv_key.is_null() && pub_key.is_null() {
+        return;
+    }
+    if pub_key.is_null() {
+        let k = RistrettoSecretKey::random(&mut OsRng);
+        convert_to_c_array(k.as_bytes(), priv_key);
+    } else {
+        let (k, p) = RistrettoPublicKey::random_keypair(&mut OsRng);
+        convert_to_c_array(k.as_bytes(), priv_key);
+        convert_to_c_array(p.as_bytes(), pub_key);
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn sign(
+    priv_key: *const KeyArray,
+    msg: *const c_char,
+    nonce: *mut KeyArray,
+    signature: *mut KeyArray,
+)
+{
+    if nonce.is_null() || signature.is_null() || priv_key.is_null() || msg.is_null() {
+        return;
+    }
+    let k = match RistrettoSecretKey::from_bytes(&(*priv_key)) {
+        Ok(k) => k,
+        _ => return,
+    };
+    let r = RistrettoSecretKey::random(&mut OsRng);
+    let msg = match CStr::from_ptr(msg).to_str() {
+        Ok(s) => s,
+        _ => return,
+    };
+    let challenge = Blake256::digest(msg.as_bytes()).to_vec();
+    let sig = match RistrettoSchnorr::sign(k, r, &challenge) {
+        Ok(sig) => sig,
+        _ => return,
+    };
+    convert_to_c_array(sig.get_public_nonce().as_bytes(), nonce);
+    convert_to_c_array(sig.get_signature().as_bytes(), signature);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn verify(
+    pub_key: *const KeyArray,
+    msg: *const c_char,
+    pub_nonce: *mut KeyArray,
+    signature: *mut KeyArray,
+) -> bool
+{
+    if pub_key.is_null() || msg.is_null() || pub_nonce.is_null() || signature.is_null() {
+        return false;
+    }
+    let pk = match RistrettoPublicKey::from_bytes(&(*pub_key)) {
+        Ok(k) => k,
+        _ => return false,
+    };
+    let r_pub = match RistrettoPublicKey::from_bytes(&(*pub_nonce)) {
+        Ok(r) => r,
+        _ => return false,
+    };
+    let sig = match RistrettoSecretKey::from_bytes(&(*signature)) {
+        Ok(s) => s,
+        _ => return false,
+    };
+    let msg = match CStr::from_ptr(msg).to_str() {
+        Ok(s) => s,
+        _ => return false,
+    };
+    let sig = RistrettoSchnorr::new(r_pub, sig);
+    let challenge = Blake256::digest(msg.as_bytes());
+    let challenge = match RistrettoSecretKey::from_bytes(challenge.as_slice()) {
+        Ok(e) => e,
+        _ => return false,
+    };
+    sig.verify(&pk, &challenge)
+}
+
+unsafe fn convert_to_c_array(src: &[u8], trg: *mut KeyArray) {
+    (*trg).copy_from_slice(src);
 }
